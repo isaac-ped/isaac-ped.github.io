715f7d9bcc1ad49855e1fed93682a5036c6b7fe8
Isaac Pedisich <iped@seas.upenn.edu>
2019-02-24 15:58:31 -0500

    first version of site

---
+++ scrolling.js
@@ -0,0 +1,43 @@
+var PAGE_PER_I = 600;
+
+var boxes = {
+    1: '#content_projects',
+    2: '#content_pubs',
+    3: '#content_resu',
+    4: '#content_cont',
+    5: '#content_git',
+};
+
+var scroll_to = function(box_i) {
+    $('body').scrollTo((box_i+1.2) * PAGE_PER_I + 10, {duration: 1000});
+}
+
+var set_box_pos = function(box_i, box, page_y) {
+    var min_box_pos = box_i * PAGE_PER_I + 100;
+
+    var x = 500  - 499  / ( 1 + Math.exp( - .01 * (page_y - min_box_pos)));
+    console.log(box.id, "x", x);
+    box.css("margin-top", x + "vh");
+    return x;
+}
+
+var onscroll = function (e) {  
+// called when the window is scrolled.  
+    var y = window.pageYOffset;
+    console.log(e);
+    $( "#counter").html(y);
+
+    set_box_pos(0, $( "#content_about"), 10000);
+    //set_box_pos(0, $( "#content_about" ), e.pageY);
+    set_box_pos(1, $( "#content_projects" ), y);
+    set_box_pos(2, $( "#content_pubs" ), y);
+    set_box_pos(3, $( "#content_resu" ), y);
+    set_box_pos(4, $( "#content_cont" ), y);
+    set_box_pos(5, $( "#content_git" ), y);
+} 
+
+$('html').on('mousewheel', onscroll)
+$('html').on('DOMMouseScroll', onscroll)
+window.onscroll = onscroll;
+
+scroll_to(0);
