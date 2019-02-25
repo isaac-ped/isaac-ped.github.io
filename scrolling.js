90cd8263d9fcf7f3143070abe6136ebd996623d3
Isaac Pedisich <iped@seas.upenn.edu>
2019-02-25 06:49:55 -0500

    added blurb and start of projects

--- scrolling.js
+++ scrolling.js
@@ -1,12 +1,19 @@
-var PAGE_PER_I = 600;
+var PAGE_PER_I = 1000;
 
 var boxes = {
     1: '#content_projects',
-    2: '#content_pubs',
-    3: '#content_resu',
-    4: '#content_cont',
-    5: '#content_git',
+    2: '#content_set',
+    3: '#content_hopfield',
+    4: '#content_memo',
+    5: '#content_pubs',
+    6: '#content_links',
 };
+
+var projects = [
+    'content_set',
+    'content_hopfield',
+    'content_memo'
+];
 
 var hamburger_on = false;
 
@@ -24,14 +31,18 @@ var scroll_to = function(box_i, noclose) {
     if (!noclose && hamburger_on) {
         toggle_hamburger();
     }
-    $('body').scrollTo((box_i+1.2) * PAGE_PER_I + 55, {duration: 1000});
+    $('body').scrollTo((box_i) * PAGE_PER_I - 200, {duration: 1000});
 }
 
 var set_box_pos = function(box_i, box, page_y) {
-    var min_box_pos = box_i * PAGE_PER_I + 100;
+    var min_box_pos = box_i * PAGE_PER_I - 1000;
 
-    var x = 2000  - 1955  / ( 1 + Math.exp( - .01 * (page_y - min_box_pos)));
-    console.log(box.id, "x", x);
+    if (projects.indexOf(box.attr('id'))>= 0) {
+        var x = 2000  - 1885  / ( 1 + Math.exp( - .01 * (page_y - min_box_pos)));
+    } else {
+        var x = 2000  - 1955  / ( 1 + Math.exp( - .01 * (page_y - min_box_pos)));
+    }
+    console.log(box.attr('id'), "x", x);
     box.css("margin-top", x + "px");
     return x;
 }
@@ -43,14 +54,13 @@ var onscroll = function (e) {
     $( "#counter").html(y);
 
     set_box_pos(0, $( "#content_about"), 10000);
-    //set_box_pos(0, $( "#content_about" ), e.pageY);
-    set_box_pos(1, $( "#content_projects" ), y);
-    set_box_pos(2, $( "#content_pubs" ), y);
-    set_box_pos(3, $( "#content_links" ), y);
+    for (var i=0; i <= 6; i++) {
+        set_box_pos(i, $(boxes[i]), y);
+    }
 } 
 
 $('html').on('mousewheel', onscroll)
 $('html').on('DOMMouseScroll', onscroll)
 window.onscroll = onscroll;
 
-scroll_to(0);
+onscroll();
